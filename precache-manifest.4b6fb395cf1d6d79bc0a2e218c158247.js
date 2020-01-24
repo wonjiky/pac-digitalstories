3e3921b4d2e3fa3631609c6c4cd01326d9baa115
WonJik Yang <yangw783@newschool.edu>
2020-01-24 12:17:21 +0100

    Updates

---
+++ precache-manifest.4b6fb395cf1d6d79bc0a2e218c158247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eff9b886e6db32ede8cadfa5539469a",
+    "url": "/pac-digitalstories/index.html"
+  },
+  {
+    "revision": "8acccb34505ab1308836",
+    "url": "/pac-digitalstories/static/css/main.58f22c0a.chunk.css"
+  },
+  {
+    "revision": "83e1958ef6c979f132b5",
+    "url": "/pac-digitalstories/static/js/2.37b73f5f.chunk.js"
+  },
+  {
+    "revision": "ea00a4417fb11c8692b2d8d118ec4f6d",
+    "url": "/pac-digitalstories/static/js/2.37b73f5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8acccb34505ab1308836",
+    "url": "/pac-digitalstories/static/js/main.46e99d59.chunk.js"
+  },
+  {
+    "revision": "071c681997ff3d203ae7",
+    "url": "/pac-digitalstories/static/js/runtime-main.d80cdff6.js"
+  }
+]);
