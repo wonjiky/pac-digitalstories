ef43a9817f0f72fb41553b0fc0c2168c16b3656d
WonJik Yang <yangw783@newschool.edu>
2020-01-24 11:54:06 +0100

    Updates

---
+++ precache-manifest.1e48373b7e5f68205024d8423584ec49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e4c166c6873f4b83412de024a57679",
+    "url": "/pac-digitalstories/index.html"
+  },
+  {
+    "revision": "e6b7421215e512651fa2",
+    "url": "/pac-digitalstories/static/css/main.05602d09.chunk.css"
+  },
+  {
+    "revision": "7763a28725685581b0c7",
+    "url": "/pac-digitalstories/static/js/2.98769110.chunk.js"
+  },
+  {
+    "revision": "4be15796f8035dbaa1a478ee31c9c671",
+    "url": "/pac-digitalstories/static/js/2.98769110.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6b7421215e512651fa2",
+    "url": "/pac-digitalstories/static/js/main.fb30a2c1.chunk.js"
+  },
+  {
+    "revision": "071c681997ff3d203ae7",
+    "url": "/pac-digitalstories/static/js/runtime-main.d80cdff6.js"
+  }
+]);
