466d6f944653c61bb91ac84f740dd0ff874965e2
Wonjik <wonjikyang@gmail.com>
2020-02-04 17:09:26 +0100

    Updates

---
+++ precache-manifest.18113e7440233426d655d8bb7777a40f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "269e49210018a20f7fae0c7faa22b8b2",
+    "url": "/pac-digitalstories/index.html"
+  },
+  {
+    "revision": "e533cae00f7fe80bf503",
+    "url": "/pac-digitalstories/static/css/main.c5695de7.chunk.css"
+  },
+  {
+    "revision": "f212ee077a7e38d8d145",
+    "url": "/pac-digitalstories/static/js/2.9cd964b1.chunk.js"
+  },
+  {
+    "revision": "4be15796f8035dbaa1a478ee31c9c671",
+    "url": "/pac-digitalstories/static/js/2.9cd964b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e533cae00f7fe80bf503",
+    "url": "/pac-digitalstories/static/js/main.28ac302b.chunk.js"
+  },
+  {
+    "revision": "071c681997ff3d203ae7",
+    "url": "/pac-digitalstories/static/js/runtime-main.d80cdff6.js"
+  }
+]);
